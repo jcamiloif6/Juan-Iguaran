53e9c4ba387cf3662302abfd488c06e059095825
jcamiloif6 <46933022+jcamiloif6@users.noreply.github.com>
2019-01-22 19:47:00 -0500

    Add files via upload

--- talleres/taller01/fecha.java
+++ talleres/taller01/fecha.java
@@ -1,26 +1,28 @@
-
 public class Fecha {
-    public Fecha() {
 
+  private int d, m, a;
+   
+    public Fecha(int dd, int mm, int aa) {
+     d = dd; m = mm; a = aa;
     }
 
     public int dia() {
-
+      return d;
     }
 
     public int mes() {
-
+      return m;
     }
 
     public int anio() {
-
+      return a;
     }
 
     // -1 si esta fecha es anterior a la otra
     // 0 si son iguales
     // 1 si esta fecha es posterior a la otra
     public int comparar(Fecha otra) {
-
+      
     }
 
     public String toString() {
