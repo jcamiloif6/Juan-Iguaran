9b451153220841eb5a1903529f37aae6c92f9edd
jcamiloif6 <46933022+jcamiloif6@users.noreply.github.com>
2019-02-19 11:57:24 -0500

    Add files via upload

--- talleres/taller05/taller5.java
+++ talleres/taller05/taller5.java
@@ -24,6 +24,24 @@ public class taller5
         // T(n) = C1 + C2 * n + C3 * n + C4
         // T(n) = O(n)
     }
+    
+    public static void ordenar(int[]a){
+        int b=0;
+    for(int i= 0; i<a.length; i++){//c1+c2*n
+      for(int j=0; j<a.length; j++) { //c3+c4*n^2
+        if(a[i]<a[j]) {//c5*n^2
+        b=a[i];//c6*n^2    
+        a[i] = a[j];//c7*n^2
+        a[j] = b;//c8*n^2
+        }
+        }
+    }
+    for(int k=0; k<a.length; k++){//c9+c10*n
+    System.out.println(a[k]);//c11*n
+    }
+    //T(n)=(c1+c2*n) + (c3+c4*n^2) + (c5*n^2) + (c6*n^2) + (c7*n^2) + (c8*n^2) + (c9+c10*n) + (c11*n)
+    //T(n) = O(n*n)
+    }
 }
 
 
